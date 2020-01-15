9bce164ed6a0d89e91608f4f5a5e814df37b8615
crispysodium <christinena.316@gmail.com>
2020-01-15 00:21:32 -0800

    added empty hour rows for user to input text

--- script.js
+++ script.js
@@ -4,25 +4,60 @@ var date = moment().format("dddd, MMMM Do")
 today.textContent = date; // current date 
 console.log(date);
 var currentHour = moment().format("hA");
+// var currentHour = 12;
 console.log(currentHour);
-var startHour = moment.min("9AM");
+var startHour = 9;
 console.log(startHour);
-var endHour = moment.max("5PM");
+var endHour = 17;
 console.log(endHour);
 
 // grabs div tag for time block container
 var display = document.querySelector(".container");
-emptyCalendar(startHour);
-
-emptyCalendar(endHour);
+emptyCalendar(startHour); //fence post
+for (let i=10; i<17; i++) {
+    var intHour = i;
+    emptyCalendar(i);
+}
+console.log(intHour);
+emptyCalendar(endHour); // fence post
 //creates new time block
-function emptyCalendar (hour) {
+function emptyCalendar (hr) {
     var timeBlock = document.createElement("div");
-    timeBlock.setAttribute("class", "row block");
+    timeBlock.setAttribute("class", "row time-block");
     var timeHr = document.createElement("div");
     timeHr.setAttribute("class", "col-sm-2 hour");
-    timeHr.textContent = hour;
+    var inputUnit = document.createElement("input");
+    inputUnit.setAttribute("type", "text");
+    if (hr == currentHour) {
+        inputUnit.setAttribute("class", "col-sm-8 present description textarea");
+        console.log("true");
+        console.log(hr);
+        console.log(currentHour);
+    } else if (hr < currentHour) {
+        console.log("false");
+        inputUnit.setAttribute("class", "col-sm-8 past description textarea");
+    } else {
+        inputUnit.setAttribute("class", "col-sm-8 future description textarea");
+    }
+    if (hr > 12) {
+        timeHr.textContent = (hr - 12) + "PM" 
+    } else {
+        timeHr.textContent = hr + "AM";
+    }
+    
+    var save = document.createElement("button");
+    save.setAttribute("class", "col-sm-2 saveBtn");
+    save.textContent = "save";
     timeBlock.appendChild(timeHr);
+    timeBlock.appendChild(inputUnit);
+    timeBlock.appendChild(save);
     display.appendChild(timeBlock);
 }
-// emptyCalendar();
+
+// var saveBtn = document.getElementsByClassName("saveBtn");
+// var userInput = document.getElementsByClassName("textarea");
+// saveBtn.addEventListener("click", function() {
+//     var saveData = userInput.value;
+
+// })
+
