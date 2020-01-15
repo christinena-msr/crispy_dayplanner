70ed674cfdf89521fec2b133715d761a0b48dce3
crispysodium <christinena.316@gmail.com>
2020-01-14 23:30:27 -0800

    added start and end time to time block container

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ today.textContent = date; // current date
 console.log(date);
 var currentHour = moment().format("hA");
 console.log(currentHour);
-var startHour = moment().hour()
+var startHour = moment.min("9AM");
+console.log(startHour);
+var endHour = moment.max("5PM");
+console.log(endHour);
 
 // grabs div tag for time block container
 var display = document.querySelector(".container");
+emptyCalendar(startHour);
 
+emptyCalendar(endHour);
 //creates new time block
-function emptyCalendar () {
+function emptyCalendar (hour) {
     var timeBlock = document.createElement("div");
     timeBlock.setAttribute("class", "row block");
-    var hour = document.createElement("div");
-    hour.setAttribute("class", "col-sm-2 hour");
-    hour.textContent = "new time block hour";
-    timeBlock.appendChild(hour);
+    var timeHr = document.createElement("div");
+    timeHr.setAttribute("class", "col-sm-2 hour");
+    timeHr.textContent = hour;
+    timeBlock.appendChild(timeHr);
     display.appendChild(timeBlock);
 }
-emptyCalendar();
+// emptyCalendar();
